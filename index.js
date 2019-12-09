9ddb40d0d90ad0b58d79d9df162ef04d0786c5fa
Esteban Garces <AnthonyGarces@users.noreply.github.com>
2019-12-09 16:04:03 -0600

    Added inquirer functionality, now asks the user for their username and favorite color

--- index.js
+++ index.js
@@ -2,4 +2,22 @@ const fs = require('fs');
 const util = require('fs');
 const axios = require('axios');
 const inquirer = require('inquirer');
-const convertFactory = require('electron-html-to')
+const convertFactory = require('electron-html-to');
+
+const questions = [
+    {
+        message: "What's your Github username?",
+        name: "Username",
+    }, 
+    {
+        message: "What's your favorite color??",
+        name: "Color",
+    },
+    
+]
+
+inquirer
+    .prompt(questions)
+    .then(function(data) {
+        console.log(data.Username + '\n' + data.Color)
+    })
