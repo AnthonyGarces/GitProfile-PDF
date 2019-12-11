e250270268fd2c28e7aa32a345e4e344cd9e7274
Esteban Garces <AnthonyGarces@users.noreply.github.com>
2019-12-10 18:33:16 -0600

    added query links for axios pulls, created function to get the starred github repo count

--- index.js
+++ index.js
@@ -16,8 +16,25 @@ const questions = [
     
 ]
 
+function getStars(y) {
+    let stargazers = 0;
+    profile.data.forEach(y => stargazers += y.stargazers_count); 
+    console.log(stargazers)
+}
+
 inquirer
     .prompt(questions)
-    .then(function(data) {
-        console.log(data.Username + '\n' + data.Color)
+    .then(data => {
+        //has name, location, bio, avatar url, url, followers, following, repos, blog, username
+        const queryUrlUser = `https://api.github.com/users/${data.Username}`;
+        //stargazers added up over each repo ? ask about github stars
+        const queryUrlRepos = `https://api.github.com/users/${data.Username}/repos?per_page=100`;
+        const queryUrlFollowers = `https://api.github.com/users/${data.Username}/followers?per_page=100`;
+
+        axios
+            .get(queryUrlUser)
+            .then(profile => {
+                
+          
+            })
     })
