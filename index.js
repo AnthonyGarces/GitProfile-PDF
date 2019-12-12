9db538bb28de14ddd0a10c08246c86fc3c3ae67d
Esteban Garces <AnthonyGarces@users.noreply.github.com>
2019-12-11 22:35:02 -0600

    styled top half of the index.html

--- index.js
+++ index.js
@@ -45,99 +45,101 @@ inquirer
                 converterPath: convertFactory.converters.PDF
             });
 
-            conversion({ html: `<!DOCTYPE html>
-            <html lang="en">
-            ​
-            <head>
-                <meta charset="UTF-8">
-                <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                <meta http-equiv="X-UA-Compatible" content="ie=edge">
-                <title>Github Profile PDF Mock-up</title>
-                <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-            </head>
-            ​
-            <body>
-            ​   <div>
-                    <div>
-                    <img
-                        src="${returns.avatar}"
-                        alt="Git Photo"
-                    >
-            
-                    <div>
-                        <h1>Hi!</h1>
-                        <h1>My name is ${returns.name}</h1>
-                        <h3>Currently @ ${returns.company}</h3>
-                        <div>
-                        <div>
-                            <img class=""
-                            src="https://google.com/maps/place/${returns.location}"
-                            alt="location logo"
-                            style = ''
-                            >
-                            Location
-                        </div>
-                        <div>
-                            <img class=""
-                            src="${returns.github}"
-                            alt="github logo"
-                            style = ''
-                            >
-                            Github
-                        </div>
-                        <div>
-                            <img class=""
-                            src="${returns.blog}"
-                            alt="feed logo"
-                            style = ''
-                            >
-                            Blog
-                        </div>
-                        </div>
-            
-                    </div>
-                    
-                    </div>
-                </div>
-            
-                <div>
-                    <h1>I build things and I teach people how to code.</h1>
-                    <br>
-                    <div>
-                    <div>
-                        <h2>Public Repositories:</h2>
-                        <h2>${returns.repos}</h2>
-                    </div>
-                    <div>
-                        <h2>Followers:</h2>
-                        <h2>${returns.followers}</h2>
-                    </div>
-                    </div>
-                    <div>
-                    <div>
-                        <h2>Github Stars:</h2>
-                        <h2>${stargazers}</h2>
-                    </div>
-                    <div>
-                        <h2>Following:</h2>
-                        <h2>${returns.following}</h2>
-                    </div>
-                    </div> 
-                </div>
-            
-                <div id="FooterDiv">
-            
-            </body>
-            ​
-            </html>`}, function(err, PDF) {
-                if (err) {
-                    return console.log(err);
-                }
+            console.log(returns)
 
-                console.log(PDF.numberOfPages);
-                console.log(PDF.logs);
-                PDF.stream.pipe(fs.createWriteStream('Profile.pdf'))
-            })
+            // conversion({ html: `<!DOCTYPE html>
+            // <html lang="en">
+            // ​
+            // <head>
+            //     <meta charset="UTF-8">
+            //     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            //     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+            //     <title>Github Profile PDF Mock-up</title>
+            //     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+            // </head>
+            // ​
+            // <body>
+            // ​   <div>
+            //         <div>
+            //         <img
+            //             src="${returns.avatar}"
+            //             alt="Git Photo"
+            //         >
+            
+            //         <div>
+            //             <h1>Hi!</h1>
+            //             <h1>My name is ${returns.name}</h1>
+            //             <h3>Currently @ ${returns.company}</h3>
+            //             <div>
+            //             <div>
+            //                 <img class=""
+            //                 src="https://google.com/maps/place/${returns.location}"
+            //                 alt="location logo"
+            //                 style = ''
+            //                 >
+            //                 Location
+            //             </div>
+            //             <div>
+            //                 <img class=""
+            //                 src="${returns.github}"
+            //                 alt="github logo"
+            //                 style = ''
+            //                 >
+            //                 Github
+            //             </div>
+            //             <div>
+            //                 <img class=""
+            //                 src="${returns.blog}"
+            //                 alt="feed logo"
+            //                 style = ''
+            //                 >
+            //                 Blog
+            //             </div>
+            //             </div>
+            
+            //         </div>
+                    
+            //         </div>
+            //     </div>
+            
+            //     <div>
+            //         <h1>I build things and I teach people how to code.</h1>
+            //         <br>
+            //         <div>
+            //         <div>
+            //             <h2>Public Repositories:</h2>
+            //             <h2>${returns.repos}</h2>
+            //         </div>
+            //         <div>
+            //             <h2>Followers:</h2>
+            //             <h2>${returns.followers}</h2>
+            //         </div>
+            //         </div>
+            //         <div>
+            //         <div>
+            //             <h2>Github Stars:</h2>
+            //             <h2>${stargazers}</h2>
+            //         </div>
+            //         <div>
+            //             <h2>Following:</h2>
+            //             <h2>${returns.following}</h2>
+            //         </div>
+            //         </div> 
+            //     </div>
+            
+            //     <div id="FooterDiv">
+            
+            // </body>
+            // ​
+            // </html>`}, function(err, PDF) {
+            //     if (err) {
+            //         return console.log(err);
+            //     }
+
+            //     console.log(PDF.numberOfPages);
+            //     console.log(PDF.logs);
+            //     PDF.stream.pipe(fs.createWriteStream('Profile.pdf'))
+            // })
         })
 
     .catch(function(err) {
